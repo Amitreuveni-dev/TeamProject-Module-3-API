026ceadf1fc74ee1aaa50068fbf8dbfa8be17999
amit reuveni <amitr123r@gmail.com>
2025-04-20 20:19:34 +0300

    add routers

---
+++ src/routers/apiQuizzes.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const router = express.Router();
+
+
+router.get("/api/users:id", (req, res) => {
+
+});
+router.get("/api/users:id/quizzes", (req, res) => {
+
+});
+router.get("/api/users:id/scores", (req, res) => {
+
+});
---
+++ src/routers/apiUsers.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const router = express.Router();
+
+
+router.post("/api/auth/register", (req, res) => {
+    
+});
+
+router.post("/api/auth/login", (req, res) => {
+    
+});
+
+router.get("/api/auth/me", (req, res) => {
+
+});
