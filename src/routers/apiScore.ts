5d0e753e25cd97ca61867b2610a729b911a7e471
amit reuveni <amitr123r@gmail.com>
2025-04-20 20:36:40 +0300

    finish score router

---
+++ src/routers/apiScore.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/api/scores/:quizId", (req, res) => {
+
+});
+
+router.get("/api/scores/user/:userId", (req, res) => {
+
+});
+
+router.get("/api/stats", (req, res) => {
+
+});
+
+router.post("/api/scores", (req, res) => {
+
+});
