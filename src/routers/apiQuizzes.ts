478303544807cbfcf9f27d7a354eda16e1bec841
amit reuveni <amitr123r@gmail.com>
2025-04-20 20:29:47 +0300

    finish quizzes router

--- src/routers/apiQuizzes.ts
+++ src/routers/apiQuizzes.ts
@@ -3,12 +3,29 @@ import express from "express";
 const router = express.Router();
 
 
-router.get("/api/users:id", (req, res) => {
+router.get("/api/quizzes", (req, res) => {
 
 });
-router.get("/api/users:id/quizzes", (req, res) => {
+
+router.get("/api/quizzes/:id", (req, res) => {
+    const { id } = req.params;
+});
+
+router.get("/api/quizzes?tag=math&sort=rating", (req, res) => {
 
 });
-router.get("/api/users:id/scores", (req, res) => {
+
+router.post("/api/quizzes/:id/submit", (req, res) => {
 
 });
+
+
+router.post("/api/quizzes", (req, res) => {
+
+});
+
+
+router.delete("/api/quizzes/:id", (req, res) => {
+    const { id } = req.params;
+});
+
--- src/routers/apiUsers.ts
+++ src/routers/apiUsers.ts
@@ -3,6 +3,18 @@ import express from "express";
 const router = express.Router();
 
 
+router.get("/api/users:id", (req, res) => {
+
+});
+router.get("/api/users:id/quizzes", (req, res) => {
+
+});
+router.get("/api/users:id/scores", (req, res) => {
+
+});
+
+
+
 router.post("/api/auth/register", (req, res) => {
     
 });
