f262447ab0452b9fc7f3f1b669cd3aef2ff49ba2
amit reuveni <amitr123r@gmail.com>
2025-04-20 20:32:33 +0300

    finish rating router

---
+++ src/routers/apiRating.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/api/ratings/quizId", (req, res) => {
+
+});
+
+router.post("/api/ratings/quizId", (req, res) => {
+
+});
+
