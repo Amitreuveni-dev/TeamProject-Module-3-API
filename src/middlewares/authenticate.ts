25c56f82b070908cfc57ab74cf21d1ed36325003
amit reuveni <amitr123r@gmail.com>
2025-04-23 18:45:43 +0300

    .

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,14 +1,13 @@
+import cookieParser from "cookie-parser";
 import { Handler } from "express";
 
 export const authenticate: Handler = (req, res, next) => {
-  console.log("Signed cookies:", req.signedCookies);
-  console.log("Unsigned cookies:", req.cookies);
-  console.log("Authenticated request from:", req.user);
 
   const userId = req.signedCookies.userId;
 
   if (!userId) {
-    res.status(401).end();
+    res.status(401);
+    res.end();
     return;
   }
 
